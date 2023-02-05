7e25b3fd0780f41cfa75eab0bdd2fdc2e599142c
IpekYurtbilen <ipekkeskek@yahoo.com>
2023-02-05 13:14:13 -0600

    calculateShippingCost method has been changed to no parameters.

--- src/ShoppingApp/Buyer.java
+++ src/ShoppingApp/Buyer.java
@@ -1,23 +1,24 @@
 package ShoppingApp;
 
-public abstract class Buyer extends User{
+public abstract class Buyer extends User {
 
-    /** This is an abstract class and inherits from User class. It has no additional variables
-     *
+    /**
+     * This is an abstract class and inherits from User class. It has no additional variables
+     * <p>
      * Create corresponding constructor to super.
-     *
+     * <p>
      * Create abstract void checkOut method with PaymentMethod parameter
      * Create abstract double calculateShippingCost method with PaymentMethod parameter
-     * */
+     */
 
 
     public Buyer(String name, String address) {
         super(name, address);
     }
 
-        public abstract void checkOut(PaymentMethod paymentMethod);
-    public abstract double calculateShippingCost(PaymentMethod paymentMethod);
+    public abstract void checkOut(PaymentMethod paymentMethod);
 
+    public abstract double calculateShippingCost();
 
 
 }
