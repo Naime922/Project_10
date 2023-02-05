314054fa335fec2e12062623f24c18218eea917d
canny <canny_02@hotmail.com>
2023-02-05 13:37:37 -0500

    I created RegularBuyer class but sellProduct method must be added

--- src/ShoppingApp/RegularBuyer.java
+++ src/ShoppingApp/RegularBuyer.java
@@ -1,18 +1,43 @@
 package ShoppingApp;
 
-public class RegularBuyer {
+
+import static ShoppingApp.PaymentMethod.CASHonDELIVERY;
+import static ShoppingApp.PaymentMethod.CREDITCARD;
+
+ public abstract class RegularBuyer extends Buyer {
+    // This class inherits from Buyer class. It has no additional variables
+    //  Create a corresponding constructor to super
+    public RegularBuyer(String name, String address) {
+        super(name, address);
+    }
+
+    /* Implement checkOut method. In the method
+     *      call sellProduct method for each product and
+     *      if payment method is CREDITCARD print "$cardTotal+shipping has been deducted from your card"
+     *      if payment method is CASHonDELIVERY print "The amount you must pay on delivery is $cardTotal+shipping"
+     */
+
+    @Override
+    public void checkOut(PaymentMethod paymentMethod) {
+        //sellProduct() method will be added for ach product
+
+        double shipping = calculateShippingCost(paymentMethod);
+        double cardTotal = calculateShippingCost() + getCart().getTotal();
+        if (paymentMethod.equals(CREDITCARD)) {
+            System.out.println("$cardTotal+shipping has been deducted from your card");
+        } else if (paymentMethod.equals(CASHonDELIVERY)) {
+            System.out.println("The amount you must pay on delivery is $cardTotal+shipping");
+        }
+    }
+
+    @Override
+    public double calculateShippingCost() {
+        if (getCart().getTotal() < 50) {
+            return getCart().getTotal() * 0.10;
+        }
+        return 0;
+    }
+
+   }
 
 
-   /** This class inherits from Buyer class. It has no additional variables
-    * Create a corresponding constructor to super.
-    *
-    * implement calculateShipping method. In the method
-    *                   if cart total is less than $50 return cartTotal*0.10
-    *
-    * Implement checkOut method. In the method
-    *                   call sellProduct method for each product and
-    *                   if payment method is CREDITCARD print "$cardTotal+shipping has been deducted from your card"
-    *                   if payment method is CASHonDELIVERY print "The amount you must pay on delivery is $cardTotal+shipping"
-    *
-    * */
-}
