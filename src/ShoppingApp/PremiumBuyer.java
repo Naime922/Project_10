aeebf983c232d781079cda3c00f546cf50a74561
umutcandan <umt.cndan@gmail.com>
2023-02-05 22:08:15 -0500

    Created premium buyer

--- src/ShoppingApp/PremiumBuyer.java
+++ src/ShoppingApp/PremiumBuyer.java
@@ -14,21 +14,26 @@ public class PremiumBuyer extends Buyer{
      * Implement calculateShippingCost method. return 0.
      * */
 
-//    public PremiumBuyer(String name, String address, Cart cart) {
-//        super(name, address, cart);
-//        this.getShippingCost() = calculateShippingCost();
-//        this.getUserType()=UserType.PREMIUM;
-//    }
-//
-//    @Override
-//    public void checkOut() {
-//        // Code copied from RegularBuyer class
-//        double totalCost = getCart().getTotal() + getShippingCost();
-//        System.out.println("Total cost for " + getName() + ": $" + totalCost);
-//    }
-//
-//    @Override
-//    public double calculateShippingCost() {
-//        return 0.0;
-//    }
+    public PremiumBuyer(String name, String address) {
+        super(name, address);
+        setShippingCost(calculateShippingCost());
+    }
+
+    @Override
+    public void checkOut(PaymentMethod paymentMethod) {
+        for (Product product : getCart().getCartProduct()) {
+            product.getSeller().sellProduct(product);
+        }
+        if (paymentMethod.equals(PaymentMethod.CREDITCARD)) {
+            System.out.println(getCart().getTotal() + calculateShippingCost() + " has been deducted from your card");
+        } else if (paymentMethod.equals(PaymentMethod.CASHonDELIVERY)) {
+            System.out.println("The amount you must pay on delivery is " + (getCart().getTotal() + calculateShippingCost()));
+        }
+
+    }
+
+    @Override
+    public double calculateShippingCost() {
+        return 0.0;
+    }
 }
