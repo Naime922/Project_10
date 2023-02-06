cf7b0a2da5e8155576d8cdf7b1d5ac3132872119
14403 <a7la_soko@yahoo.com>
2023-02-05 21:43:51 -0500

    fixed

--- src/ShoppingApp/RegularBuyer.java
+++ src/ShoppingApp/RegularBuyer.java
@@ -5,41 +5,46 @@ import static ShoppingApp.PaymentMethod.CASHonDELIVERY;
 import static ShoppingApp.PaymentMethod.CREDITCARD;
 
  public class RegularBuyer extends Buyer {
-    // This class inherits from Buyer class. It has no additional variables
-    //  Create a corresponding constructor to super
-    public RegularBuyer(String name, String address) {
-        super(name, address);
-    }
+     // This class inherits from Buyer class. It has no additional variables
+     //  Create a corresponding constructor to super
+     public RegularBuyer(String name, String address) {
+         super(name, address);
+     }
 
-    /* Implement checkOut method. In the method
-     *      call sellProduct method for each product and
-     *      if payment method is CREDITCARD print "$cardTotal+shipping has been deducted from your card"
-     *      if payment method is CASHonDELIVERY print "The amount you must pay on delivery is $cardTotal+shipping"
-     */
+     /* Implement checkOut method. In the method
+      *      call sellProduct method for each product and
+      *      if payment method is CREDITCARD print "$cardTotal+shipping has been deducted from your card"
+      *      if payment method is CASHonDELIVERY print "The amount you must pay on delivery is $cardTotal+shipping"
+  }    */
 
-    @Override
-    public void checkOut(PaymentMethod paymentMethod) {
-        //sellProduct() method will be added for ach product
-        for (Product product: getCart().getCartProduct()) {
-            product.getSeller().sellProduct(product);
+     @Override
+     public void checkOut(PaymentMethod paymentMethod) {
+         //sellProduct() method will be added for ach product
+         for (Product product : getCart().getCartProduct()) {
+             product.getSeller().sellProduct(product);
+         }
+             if (paymentMethod.equals(PaymentMethod.CREDITCARD)) {
+                 System.out.println(getCart().getTotal() + calculateShippingCost() + " has been deducted from your card");
+             } else if (paymentMethod.equals(PaymentMethod.CASHonDELIVERY)) {
+                 System.out.println("The amount you must pay on delivery is " + (getCart().getTotal() + calculateShippingCost()));
+             }
 
-            if(paymentMethod.equals(PaymentMethod.CREDITCARD)){
-                System.out.println(getCart().getTotal()+calculateShippingCost() + " has been deducted from your card");
-            } else if (paymentMethod.equals(PaymentMethod.CASHonDELIVERY)) {
-                System.out.println("The amount you must pay on delivery is " + (getCart().getTotal()+calculateShippingCost()));
-            }
 
+     }
 
      // implement calculateShipping method. In the method
-    //          if cart total is less than $50 return cartTotal*0.10
-    @Override
-    public double calculateShippingCost() {
-        if (getCart().getTotal() < 50) {
-            return getCart().getTotal() * 0.10;
-        }
-        return 0;
-    }
+     //          if cart total is less than $50 return cartTotal*0.10
+     @Override
+     public double calculateShippingCost() {
+         if (getCart().getTotal() < 50) {
+             return getCart().getTotal() * 0.10;
+         }
+         return 0;
+     }
+ }
 
-   }
+
+
+
 
 
